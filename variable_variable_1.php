9a765bdf4dea7e3301172bb31e080188f55a44f1
aulia2424 <auliamartha243@gmail.com>
2022-10-05 15:34:16 +0700

    add file variable dan sumur.php

---
+++ variable_variable_1.php
@@ -0,0 +1,7 @@
+<?php
+// luas persgi panjang
+$panjang = 10;
+$lebar = 10;
+$luas_persegi_panjang = $panjang * $lebar;
+print "hasil dari perhitungan \n p = 10\n n = 10 \n" . "luasnya =" .$luas_persegi_panjang
+?>
